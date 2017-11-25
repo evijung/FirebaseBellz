909112e2ebae09a92dc1965231f96fc85f5ad8b6
evijung <evides01@gmail.com>
2017-11-25 11:58:38 +0700

    Design UI Main Fragment Finish

--- app/src/main/java/com/th/samsen/tunyaporn/firebasebellz/MainActivity.java
+++ app/src/main/java/com/th/samsen/tunyaporn/firebasebellz/MainActivity.java
@@ -3,11 +3,23 @@ package com.th.samsen.tunyaporn.firebasebellz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.th.samsen.tunyaporn.firebasebellz.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+//            Add to Activity
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+
+
+        }
+
+
     }
 }
