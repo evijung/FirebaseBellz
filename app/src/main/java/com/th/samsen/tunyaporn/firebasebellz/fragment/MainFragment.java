ce6a31804cd7114d2b08d6c860766897a2fd2616
evijung <evides01@gmail.com>
2017-11-25 14:28:52 +0700

    Design UI Register Fragment Finish

--- app/src/main/java/com/th/samsen/tunyaporn/firebasebellz/fragment/MainFragment.java
+++ app/src/main/java/com/th/samsen/tunyaporn/firebasebellz/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.th.samsen.tunyaporn.firebasebellz.R;
 
@@ -14,10 +15,24 @@ import com.th.samsen.tunyaporn.firebasebellz.R;
  */
 
 public class MainFragment extends Fragment{
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+//        Register Controller
+        registerController();
+
+    }
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
+            }
+        });
     }
 
     @Nullable
@@ -25,5 +40,5 @@ public class MainFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
-    }
+    }//Main Method
 }
